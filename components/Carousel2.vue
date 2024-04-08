d7af40148c29a70c442451a6f9f9c59d2929f8d1
KevinYabut08 <Kevin.Yabut@edu.sait.ca>
2024-04-08 13:10:23 +0800

    added another carousel component

--- components/Carousel2.vue
+++ components/Carousel2.vue
@@ -1,3 +1,33 @@
 <script></script>
-<template></template>
+<template>
+  <Swiper
+    :height="300"
+    :modules="[SwiperAutoplay, SwiperEffectCreative]"
+    :slides-per-view="1"
+    :loop="true"
+    :effect="'creative'"
+    :autoplay="{
+      delay: 8000,
+      disableOnInteraction: true,
+    }"
+    :creative-effect="{
+      prev: {
+        shadow: false,
+        translate: ['-20%', 0, -1],
+      },
+      next: {
+        translate: ['100%', 0, 0],
+      },
+    }"
+  >
+    <SwiperSlide
+      v-for="(slide, idx) in slides"
+      :key="idx"
+      :style="`background-color: ${slide.bg}; color: ${slide.color}`"
+    >
+      {{ idx }}
+    </SwiperSlide>
+    <SwiperControls />
+  </Swiper>
+</template>
 <style></style>
