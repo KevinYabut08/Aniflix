797f6b1275fe7bc4d56aba6323e77454e3347cbf
KevinYabut08 <Kevin.Yabut@edu.sait.ca>
2024-04-10 01:11:49 +0800

    added carousel

--- components/Carousel2.vue
+++ components/Carousel2.vue
@@ -1,29 +1,85 @@
 <script></script>
 <template>
   <Swiper
-    class="relative"
-    :width="700"
-    :modules="[SwiperAutoplay, SwiperEffectCards]"
+    :height="1000"
+    :modules="[SwiperAutoplay, SwiperEffectCreative]"
     :slides-per-view="1"
     :loop="true"
-    :effect="'cards'"
+    :effect="'creative'"
     :autoplay="{
       delay: 5000,
       disableOnInteraction: true,
     }"
+    :creative-effect="{
+      prev: {
+        shadow: false,
+        translate: ['-20%', 0, -1],
+      },
+      next: {
+        translate: ['100%', 0, 0],
+      },
+    }"
   >
-    <SwiperSlide class="absolute">
-      <img src="assets/images/jujutsu-kaisen.webp" />
+    <SwiperSlide
+      ><iframe
+        width="1000"
+        height="600"
+        src="https://www.youtube.com/embed/e8hQsNouBR4?si=Z1sQuMHkllJiW1DR"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </SwiperSlide>
+    <SwiperSlide
+      ><iframe
+        width="1000"
+        height="600"
+        src="https://www.youtube.com/embed/2Yk-68Fx_HM?si=CpEgWwPXwD9JeRdw"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </SwiperSlide>
+    <SwiperSlide
+      ><iframe
+        width="1000"
+        height="600   "
+        src="https://www.youtube.com/embed/6xWfTkL4MEE?si=JPjypLUoe3veRU_u"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </SwiperSlide>
+    <SwiperSlide
+      ><iframe
+        width="1000"
+        height="600"
+        src="https://www.youtube.com/embed/DcofRCnm20E?si=iepFX-LESco2JfXT"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </SwiperSlide>
     <SwiperSlide>
-      <img src="assets/images/one-punch-man.jpg" />
-    </SwiperSlide>
-    <SwiperSlide>
-      <img src="/assets/images/one-piece-egghead-anime-poster.webp" />
-    </SwiperSlide>
-    <SwiperSlide>
-      <img src="/assets/images/bluelock-1024x538.png" />
-    </SwiperSlide>
+      <iframe
+        width="1000"
+        height="600"
+        src="https://www.youtube.com/embed/ZK3qs1CWrMg?si=cF4paklJZ9PsC9tj"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe
+    ></SwiperSlide>
   </Swiper>
 </template>
 <style></style>
