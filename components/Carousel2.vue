011321efd0ca0bf2366021aa20d6eb4fcb4b384a
KevinYabut08 <Kevin.Yabut@edu.sait.ca>
2024-04-10 00:07:13 +0800

    added carousel

--- components/Carousel2.vue
+++ components/Carousel2.vue
@@ -1,38 +1,29 @@
 <script></script>
 <template>
   <Swiper
-    :height="300"
-    :modules="[SwiperAutoplay, SwiperEffectCreative]"
+    class="relative"
+    :width="700"
+    :modules="[SwiperAutoplay, SwiperEffectCards]"
     :slides-per-view="1"
     :loop="true"
-    :effect="'creative'"
+    :effect="'cards'"
     :autoplay="{
-      delay: 8000,
+      delay: 5000,
       disableOnInteraction: true,
     }"
-    :creative-effect="{
-      prev: {
-        shadow: false,
-        translate: ['-20%', 0, -1],
-      },
-      next: {
-        translate: ['100%', 0, 0],
-      },
-    }"
   >
+    <SwiperSlide class="absolute">
+      <img src="assets/images/jujutsu-kaisen.webp" />
+    </SwiperSlide>
     <SwiperSlide>
-      <img src="/assets/images/one-piece-egghead-anime-poster.webp" />
+      <img src="assets/images/one-punch-man.jpg" />
     </SwiperSlide>
     <SwiperSlide>
       <img src="/assets/images/one-piece-egghead-anime-poster.webp" />
     </SwiperSlide>
     <SwiperSlide>
-      <img src="/assets/images/one-piece-egghead-anime-poster.webp" />
+      <img src="/assets/images/bluelock-1024x538.png" />
     </SwiperSlide>
-    <SwiperSlide>
-      <img src="/assets/images/one-piece-egghead-anime-poster.webp" />
-    </SwiperSlide>
-    <SwiperControls />
   </Swiper>
 </template>
 <style></style>
